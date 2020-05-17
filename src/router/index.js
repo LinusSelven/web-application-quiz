eee9b17c70fafa91823390b3edc78f0cc5461a77
Aisha-Alsafi <64260528+Aisha-Alsafi@users.noreply.github.com>
2020-05-18 01:35:54 +0200

    Rate us component

--- src/router/index.js
+++ src/router/index.js
@@ -33,6 +33,15 @@ const routes = [
         // this generates a separate chunk (about.[hash].js) for this route
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "about" */ '../views/login.vue')
+    }  ,
+
+    {
+        path: '/rate',
+        name: 'rate',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/rate.vue')
     },
 
     {
---
+++ src/views/rate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+        <h1>This is a Rate us page</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "rate"
+    }
+</script>
+
+<style scoped>
+
+</style>
