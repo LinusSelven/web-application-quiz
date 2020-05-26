51e13abb841f1e5bbb125c7c14c743e26f21ac16
Aisha-Alsafi <64260528+Aisha-Alsafi@users.noreply.github.com>
2020-05-26 12:36:43 +0200

    svenska quiz

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,14 @@ const routes = [
         path: '/matteQuiz',
         name: 'mattequiz',
         component: () => import(/* webpackChunkName: "about" */ '../components/mattequiz.vue')
+    },
+
+    {
+        path: '/svenskaQuiz',
+        name: 'svenskaQuiz',
+        component: () => import(/* webpackChunkName: "about" */ '../components/svenskaQuiz.vue')
     }
+
 
 ]
 
