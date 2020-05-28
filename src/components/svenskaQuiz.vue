73f98623954cfaedb766d0f02c1a6ff82c52758a
Linus S <kpOC8&1I3$Js&03*bWN8>
2020-05-28 14:14:32 +0200

    early stage of trying to fix drag and drop

--- src/components/svenskaQuiz.vue
+++ src/components/svenskaQuiz.vue
@@ -1,85 +1,74 @@
 <template>
-    <div class="about">
-        <div v-if="!isDone" class="q-question">
-            <h1>Quiz Level: {{selectedLevel}} / {{quizLevel.length}}   |    Question: {{questionNumber+1}} / {{geoQuiz.length}}</h1>
-            <div>
-                <h2>{{geoQuiz[questionNumber].quizQuestion}}</h2>
-            </div>
-            <div class="q-img">
-                <h2>
-                    <img :src="getImgUrl(geoQuiz[questionNumber].quizImg)" v-bind:alt="pic">
-                </h2>
-            </div>
+    <div class="svenskaquiz">
+        <h1>HELLO</h1>
+        <div class="q-question">
             <div class="q-answer">
-                <button class="q-btn" @click="userChoseAnswer($event) " :disabled="userHasGuessed" value="1">{{geoQuiz[questionNumber].quizAnswer1}}
-                </button>
-                <button class="q-btn" @click="userChoseAnswer($event)" :disabled="userHasGuessed" value="2">{{geoQuiz[questionNumber].quizAnswer2}}
-                </button>
-                <button class="q-btn" @click="userChoseAnswer($event)" :disabled="userHasGuessed" value="3">{{geoQuiz[questionNumber].quizAnswer3}}
-                </button>
+                <div class="part"><p>{{svenskaQuiz[0].quizPart1}}</p></div>
+                <div class="empty">DRAG SPACE</div>
+                <div class="part"><p>{{svenskaQuiz[0].quizPart2}}</p></div>
+                <div class="empty">DRAG SPACE</div>
+                <div class="part"><p>{{svenskaQuiz[0].quizPart3}}</p></div>
+                <div class="empty">DRAG SPACE</div>
+                <div class="part"><p>{{svenskaQuiz[0].quizEnd}}</p></div>
             </div>
-        </div>
-        <div v-if="isDone" class="q-question">
-            <img src="../assets/result1.png" alt="res" class="res">
-            <h1>{{finalScore}} %</h1>
-            <button class="q-btn" @click="nextLevelQuiz">Next Quiz</button>
-            <button class="q-btn" @click="redoQuiz">Last Quiz</button>
-            <h2>{{nextQuizMessage}}</h2>
         </div>
     </div>
 </template>
 
 <script>
-  export default {
-    name: "svenskaQuiz",
-    data: function () {
-      return {
-        geoQuiz: [],
-        questionNumber: 0,
-        countOfCorrectAnswers: 0,
-        userHasGuessed: false,
-        key:'0',
-        resultat:'',
-      }
-    },
+    export default {
+        name: "svenskaQuiz",
+        data: function () {
+            return {
+                svenskaQuiz: [{
+                    "quizId": 1, "quizPart1": "Jag ", "quizPart2": "ut, för att ", "quizPart3": "till min ",
+                    "quizEnd": ".", "quizAnswer1": "gick", "quizAnswer2": "handla", "quizAnswer3": "mormor"
+                }],
+                questionNumber: 0,
+                countOfCorrectAnswers: 0,
+                userHasGuessed: false,
+                key: '0',
+                resultat: '',
+            }
+        },
 
-    methods: {
-      nextQuestion: function () {
-        this.userHasGuessed = false;
-        this.resultat = '';
-        this.key='0';
-        this.questionNumber += 1;
-      },
+        methods: {
+            nextQuestion: function () {
+                this.userHasGuessed = false;
+                this.resultat = '';
+                this.key = '0';
+                this.questionNumber += 1;
+            },
 
 
-      userChoseAnswer: function (event) {
-        this.userHasGuessed = true;
-        if (event.target.value == this.geoQuiz[this.questionNumber].quizCorrectAnswer) {
-          this.countOfCorrectAnswers += 1;
-          return this.resultat = 'Rätt svar!';
-        } else {
-          return this.resultat = 'Fel svar!';
+            userChoseAnswer: function (event) {
+                this.userHasGuessed = true;
+                if (event.target.value == this.geoQuiz[this.questionNumber].quizCorrectAnswer) {
+                    this.countOfCorrectAnswers += 1;
+                    return this.resultat = 'Rätt svar!';
+                } else {
+                    return this.resultat = 'Fel svar!';
+                }
+            },
+
+
+            getImgUrl: function (pic) {
+                return require('../assets/' + pic)
+            }
+        },
+
+        mounted() {
+            fetch('http://127.0.0.1:3000/api/geoQuiz/')
+                .then((response) => {
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data.geoQuiz);
+                    this.geoQuiz = data.geoQuiz;
+                });
         }
-      },
 
-
-      getImgUrl: function (pic) {
-        return require('../assets/' + pic)
-      }
-    },
-
-    mounted() {
-      fetch('http://127.0.0.1:3000/api/geoQuiz/')
-        .then((response) => {
-          return response.json();
-        })
-        .then((data) => {
-          console.log(data.geoQuiz);
-          this.geoQuiz = data.geoQuiz;
-        });
     }
-
-  }
 </script>
 
 
@@ -100,26 +89,46 @@
         cursor: pointer;
     }
 
-    .q-question {
-        color: #02b3b3;
+    .svenskaquiz{
+        background: rgba(0, 0, 0, .7);
+        border-radius: 10px;
+        display: inline-block;
+        text-align: center;
+        width: 100%;
     }
+
+    .q-question {
+        display: table;
+        width: 100%;
+        border: 1px solid black;
+        border-right: none;
+        box-sizing: border-box;
+    }
+    .q-question > .q-answer {
+        display: table-row;
+        background-color: green;
+    }
+
+    .q-question > .q-answer > .empty {
+        background: white;
+        border-right: 1px solid black;
+        display: table-cell;
+        text-align: center;
+    }
+
 
     .q-img {
         margin: 0 auto;
         width: 200px;
         height: 200px;
     }
-    img{
+
+    img {
         max-width: 100%;
         max-height: 100%;
         display: block;
     }
 
-    .q-answer {
-        text-align: center;
-        margin-left: auto;
-        margin-right: auto;
-    }
 
     .q-btn {
         width: 100%;
@@ -133,42 +142,41 @@
         border: 1px solid rgb(7, 172, 172);
         border-radius: 4px;
     }
+
     .q-btn:hover {
         background-color: #e9e608;
         color: black;
         cursor: pointer;
     }
-    .about{
-        background: rgba(0, 0, 0, .7);
-        border-radius: 10px;
-        display: inline-block;
-        text-align: center;
-        width: 100%;
-    }
-    p{
-        margin-bottom: 20px;
-    }
-    h2, h3{
+
+
+
+    h2, h3 {
         font-family: "Times New Roman", monospace;
         color: wheat;
     }
-    .right-answer{
+
+    .right-answer {
         color: #06d4ee;
     }
+
     /* Mobile */
     @media screen and (max-width: 400px) {
     }
+
     /* Tablet */
     @media screen and (min-width: 768px) and (max-width: 1024px) {
     }
+
     /* Desktop */
     @media screen and (min-width: 1025px) {
-        .about{
+        .about {
             display: table-cell;
             text-align: center;
             vertical-align: middle;
             background: rgba(0, 0, 0, 0.7);
         }
+
         .q-btn {
             width: 30%;
             height: 50px;
