bdc0b73ec8952e812a5cb2498d2df78f3d9b9ea1
Halimove01 <halim.dakir@gmail.com>
2020-06-04 08:34:46 +0200

    make about us page suits all screens

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,14 +1,9 @@
 <template>
   <div class="about">
+    <h1>ABOUT US</h1>
     <img src="../assets/iths.png" alt="iths" class="iths">
-
-
-
-    <h3> Vi studerar till Java utvecklare i IT-HÖGSKOLA i Göteborg.</h3>
-    <p>
-      Vi är expert på världens största programmeringsspråk Java men kan också databashantering och webbutveckling som gör oss till fullstack-utvecklare.    </p>
-    <p>
-      <b>SkolQuizzen</b> är en hemsida för elever i grundskola som kan studera Geografi, Matte, Svenska och  Engelska  hemifrån.
+    <h3> Vi studerar till Java utvecklare i IT-HÖGSKOLA i Göteborg</h3>
+    <p><b>SkolQuizzen</b> är en hemsida för elever i grundskola som kan studera Geografi, Matte, Svenska och  Engelska  hemifrån.
       <br/>
       I SkolQuizen i grupp utvecklar vi en webbapplikation i form av ett Quiz   där vi gör en förstudie och planering för att därefter genomföra ett
       projekt med<b> agila metoder </b>där vi utgår från<b> Scrum</b>. Webbapplikationen görs med<b> HTML, CSS,
@@ -44,26 +39,44 @@
 
 
   .about{
-    background: rgba(0, 0, 0, .7);
-    display: inline-block;
+    display: table-cell;
     text-align: center;
-    padding-left: 50px;
-    padding-right: 50px;
+    vertical-align: top;
+    background: rgba(0, 0, 0, 0.8);
+    padding-bottom: 10px;
   }
   h2, h3{
     font-size: large;
+    text-align: justify-all;
     color: wheat;
+  }
+  h1{
+    font-family: Calibri, monospace;
+    color: wheat;
+    background-color: rgba(0, 0, 0, 0.9);
+    padding: 5px;
+    margin: auto;
   }
   p{
     color: wheat;
-    text-align: left;
+    text-align: justify-all;
+    margin: auto;
 
+  }
+  .iths{
+    margin: auto;
   }
   /* Mobile */
   @media screen and (max-width: 400px) {
   }
   /* Tablet */
   @media screen and (min-width: 768px) and (max-width: 1024px) {
+    .about{
+      display: table-cell;
+      text-align: center;
+      vertical-align: top;
+      background: rgba(0, 0, 0, 0.89);
+    }
   }
   /* Desktop */
   @media screen and (min-width: 1025px) {
@@ -72,8 +85,6 @@
       text-align: center;
       vertical-align: top;
       background: rgba(0, 0, 0, 0.8);
-      width: 100%;
-
     }
     h1{
       padding: 13px;
